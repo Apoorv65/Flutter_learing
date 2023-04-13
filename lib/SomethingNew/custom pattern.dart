be079d9aee5bd44be848a8732ec55bbbbf97fb67
apoorv <apoorvmat6@gmail.com>
2023-04-13 16:00:24 +0530

    Polygon Clipper

--- lib/SomethingNew/custom pattern.dart
+++ lib/SomethingNew/custom pattern.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-//additional run args  "--no-sound-null-safety"
+
 void main() {
   runApp(const MyApp());
 }
--- lib/SomethingNew/shapePolygonClipper.dart
+++ lib/SomethingNew/shapePolygonClipper.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:polygon_clipper/polygon_clipper.dart';
 
+//additional run args  "--no-sound-null-safety"
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
